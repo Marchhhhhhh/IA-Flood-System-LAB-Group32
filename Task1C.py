c97001ae26e6501ca03e6eae864e5d182f0d4805
Sam Mossop <sm2625@cam.ac.uk>
2022-01-22 17:23:58 +0000

    Some code written - not working just yet

--- Task1C.py
+++ Task1C.py
@@ -1,8 +1,19 @@
 from floodsystem.geo import stations_within_radius
+from floodsystem.stationdata import build_station_list
 
 def run():
 
     """ Requirements for Task 1C """
 
-    
+    stations = build_station_list()
+    centre = (52.2053, 0.1218)
+    r = 5
+    stations_within_range = []
+
+    stations_within_radius(stations, centre, r)
+
+    print(stations_within_range)
+    return stations_within_range
+
+run()
 
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -13,5 +13,15 @@ from haversine import haversine, Unit
 
 # Building function for Task 1C
 def stations_within_radius(stations, centre, r):
+    "A function which given an input of a list of stations (Type Monitoring Station), a centre co-ordinate and a radius, returns all station names within the radius of the centre."
     stations = build_station_list()
+
+    stations_within_range = []
     
+    for i in range(len(stations)):
+        distance = haversine(centre, stations[i].coord)
+
+        if distance < r:
+            stations_within_range.append(stations[i].name)
+    
+    return stations_within_range
