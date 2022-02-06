724f57437fc18edd13025bb4789eef43b4eaa79e
Steven Ma <cm2122@cam.ac.uk>
2022-02-06 01:13:58 +0000

    1B Finished

--- Task1B.py
+++ Task1B.py
@@ -5,7 +5,7 @@ from floodsystem.stationdata import build_station_list
 def run():
     """Requirements for Task 1B"""
 
-    # Build list of stations
+    # Building list of stations
     stations = build_station_list()
     x = stations_by_distance(stations,(52.2053, 0.1218))
     print(x[:10])
