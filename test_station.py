610c0387eef59d266f01b57b4f27c937cf6ea57d
Sam Mossop <sm2625@cam.ac.uk>
2022-02-07 19:58:22 +0000

    change

--- test_station.py
+++ test_station.py
@@ -3,17 +3,17 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
-from floodsystem.station import MonitoringStation
+from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
 
 
-def test_create_monitoring_station():
+def test_create_monitoring_station(n):
 
     # Create a station
-    s_id = "test-s-id"
-    m_id = "test-m-id"
-    label = "some station"
+    s_id = "test-{}-id".format(n)
+    m_id = "test-{}-id".format(n)
+    label = "Station {}".format(n)
     coord = (-2.0, 4.0)
-    trange = (-2.3, 3.4445)
+    trange = (-2.3+10*n, 3.4445+5*n)
     river = "River X"
     town = "My Town"
     s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
