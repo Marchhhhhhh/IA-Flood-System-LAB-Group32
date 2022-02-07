973926f32a47966871c63d742d7500d68fe33418
Steven Ma <cm2122@cam.ac.uk>
2022-02-07 02:26:39 +0000

    Change in test function

--- test_geo.py
+++ test_geo.py
@@ -5,7 +5,7 @@ def test_station_by_distance():
     stations = build_station_list()
     a = stations_by_distance(stations, (52.2053, 0.1218))
     b = [('Cambridge Jesus Lock', 'Cambridge', 0.840237595667494), ('Bin Brook', 'Cambridge', 2.502277543239629)]
-    c = [('Penzance Alverton', 'Penzance', 458.57727568406375), ('Penberth', 'Penberth', 467.53431870130544)]
+    c = [('Boscadjack', 'Wendron', 440.00325604140033), ('Gwithian', 'Gwithian', 442.0555261735786), ('Helston County Bridge', 'Helston', 443.3788620846717)]
     assert a[:2] == b
     assert a[:-2] == c
     
