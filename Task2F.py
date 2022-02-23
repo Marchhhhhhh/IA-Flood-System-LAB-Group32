6532f2be8fa836996b15d9d2cededbe5969f4313
Sam Mossop <sm2625@cam.ac.uk>
2022-02-23 11:32:12 +0000

    Demonstration program written realrun() in 2F, just need to do 2C to run 2F

--- Task2F.py
+++ Task2F.py
@@ -3,6 +3,7 @@ from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.analysis import polyfit
 from floodsystem.plot import plot_water_level_with_fit
 from floodsystem.stationdata import build_station_list
+from floodsystem.flood import stations_highest_rel_level
 import datetime
 
 def run():
@@ -26,7 +27,7 @@ def run():
 def run2():
 
     stations = build_station_list()
-    station_name = "Cam"
+    station_name = "Castle Foregate"
 
     for station in stations:
         if station.name == station_name:
@@ -39,3 +40,15 @@ def run2():
 
 print(run())
 run2()
+
+
+def realrun():
+
+    stations = build_station_list()
+    stations_to_plot = stations_highest_rel_level(stations, 5)
+
+    for item in stations_to_plot:
+        dates, levels = fetch_measure_levels(item.measure_id, datetime.timedelta(days=2))
+        plot_water_level_with_fit(item, dates, levels, 4)
+    
+    
