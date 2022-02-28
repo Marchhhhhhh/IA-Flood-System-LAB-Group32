9c1d875f543860e2adeeac72510a513f4f7cf217
Sam Mossop <sm2625@cam.ac.uk>
2022-02-28 00:38:17 +0000

    2C test done

--- test_flood.py
+++ test_flood.py
@@ -1,6 +1,6 @@
 from floodsystem.utils import sorted_by_key
 from floodsystem.station import MonitoringStation
-from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import stations_highest_rel_level, stations_level_over_threshold
 
 def create_test_monitoring_station(n):
 
@@ -40,3 +40,39 @@ def test_stations_level_over_threshold():
       a.append("{} {}".format(s[0].name, s[1]))
     assert a == ['Station 3 5.233333333333333', 'Station 1 0.9']
     assert len(results) == 2
+
+
+def test_stations_highest_rel_level():
+
+    stations = []
+
+    for i in range(5):
+        stations.append(create_test_monitoring_station(i))
+    
+    stations[0].latest_level = 5
+    stations[1].latest_level = 2
+    stations[2].latest_level = None
+    stations[3].latest_level = 10
+    stations[4].latest_level = 7
+
+    stations[0].typical_range = (0, 1)
+    stations[1].typical_range = (0, 1)
+    stations[2].typical_range = (0, 1)
+    stations[3].typical_range = (0, 1)
+    stations[4].typical_range = (0, 1)
+
+    stations[0].name = "Revenge of the Sith"
+    stations[1].name = "Phantom Menace"
+    stations[2].name = "Attack of the Clones"
+    stations[3].name = "Empire Strikes Back"
+    stations[4].name = "A New Hope"
+
+    a = stations_highest_rel_level(stations, 3)
+
+    b = []
+
+    for item in a:
+        b.append((item.name, item.relative_water_level()))
+    
+    assert b == [('Empire Strikes Back', 10.0), ('A New Hope', 7.0), ('Revenge of the Sith', 5.0)]
+    
